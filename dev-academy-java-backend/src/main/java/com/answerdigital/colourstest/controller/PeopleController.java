15088fcb158e0dc63f5b790ecdeb41b5c07c6856
Lucas Arvelo <lucasarvelo@gmail.com>
2019-06-20 03:16:16 -0400

    Challenges 1 to 3 + Optional

--- dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/controller/PeopleController.java
+++ dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/controller/PeopleController.java
@@ -1,18 +1,20 @@
 package com.answerdigital.colourstest.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.answerdigital.colourstest.dto.PersonUpdateDTO;
-import com.answerdigital.colourstest.exception.NotImplementedException;
 import com.answerdigital.colourstest.model.Person;
 import com.answerdigital.colourstest.repository.PeopleRepository;
 
@@ -21,7 +23,7 @@ import com.answerdigital.colourstest.repository.PeopleRepository;
 public class PeopleController {
 
     @Autowired
-    private PeopleRepository peopleRespository;
+    private PeopleRepository peopleRepository;
 
     @GetMapping
     public ResponseEntity<List<Person>> getPeople() {
@@ -31,7 +33,8 @@ public class PeopleController {
         // of people from the PeopleRepository. If there are zero
         // people returned from PeopleRepository then an empty
         // JSON array should be returned.
-        throw new NotImplementedException();
+    	
+        return new ResponseEntity<List<Person>>(peopleRepository.findAll(),  HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
@@ -42,8 +45,13 @@ public class PeopleController {
         // from the PeopleRepository based on the id parameter.
         // If null is returned from the PeopleRepository with
         // the supplied id then a NotFound should be returned.
-
-        throw new NotImplementedException();
+    	
+    	Optional<Person> person = peopleRepository.findById(id);
+    	
+    	if (person.isPresent()) {
+    		return new ResponseEntity(person, HttpStatus.OK);		
+    	}
+    	return new ResponseEntity("[]", HttpStatus.NOT_FOUND);
     }
 
     @PutMapping("/{id}")
@@ -56,7 +64,33 @@ public class PeopleController {
         // updated, the person should be returned from the endpoint.
         // If null is returned from the PeopleRepository then a
         // NotFound should be returned.
-        throw new NotImplementedException();
+    	
+        Optional<Person> person = peopleRepository.findById(id);
+        
+        if (person.isPresent()) {
+        	if (!person.equals(personUpdate)) {
+        		person.ifPresent(x -> x.setAuthorised(personUpdate.isAuthorised()));
+        		person.ifPresent(x -> x.setEnabled(personUpdate.isEnabled()));
+        		person.ifPresent(x -> x.setColours(personUpdate.getColours()));
+        		person.ifPresent(x -> peopleRepository.save(x));
+        		
+        		return new ResponseEntity(person, HttpStatus.OK);
+        	}else {
+        		return new ResponseEntity(person, HttpStatus.OK);
+        	}
+        }
+        
+        return new ResponseEntity("[]", HttpStatus.NOT_FOUND);
     }
-
+    
+    @PostMapping
+    public ResponseEntity<Person> setPerson(@RequestBody Person person) {
+    	
+    	if(person != null) {
+    		Person newPerson = peopleRepository.save(person);
+    		return new ResponseEntity(newPerson, HttpStatus.OK);
+    	}
+    	
+    	return new ResponseEntity("[]", HttpStatus.BAD_REQUEST);
+    }
 }
