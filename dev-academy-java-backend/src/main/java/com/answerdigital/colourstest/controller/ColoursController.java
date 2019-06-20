a0ef7873ea3d5b49a42891de178b7e2173ab04d1
Lucas Arvelo <lucasarvelo@gmail.com>
2019-06-20 03:18:22 -0400

    Optional Challenges

--- dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/controller/ColoursController.java
+++ dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/controller/ColoursController.java
@@ -3,10 +3,15 @@ package com.answerdigital.colourstest.controller;
 import com.answerdigital.colourstest.model.Colour;
 import com.answerdigital.colourstest.repository.ColoursRepository;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +26,28 @@ public class ColoursController {
     public ResponseEntity<List<Colour>> getColours() {
         return new ResponseEntity(coloursRepository.findAll(), HttpStatus.OK);
     }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Colour> getColour(@PathVariable("id") long id) {
+    	
+    	Optional<Colour> colour = coloursRepository.findById(id);
+    	
+    	if (colour.isPresent()) {
+    		return new ResponseEntity(colour, HttpStatus.OK);		
+    	}
+    	return new ResponseEntity("[]", HttpStatus.NOT_FOUND);
+    }
+    
+    @PostMapping
+    public ResponseEntity<Colour> setColour(@RequestBody Colour colour) {
+    	
+    	if(colour != null) {
+        	Colour newColour = coloursRepository.save(colour);
+        	return new ResponseEntity(newColour, HttpStatus.OK);
+        }
+        
+    	return new ResponseEntity("[]", HttpStatus.BAD_REQUEST);
+       
+    }
+    
 }
